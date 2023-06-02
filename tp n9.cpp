d4a516bad2cb7c0dad02c4805b6e0c2bb3587a82
sebastianallegri01 <132062777+sebastianallegri01@users.noreply.github.com>
2023-06-02 16:38:00 -0300

    Add files via upload

---
+++ tp n9.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+	string f;
+	int e;
+	
+	cout<<"Ingresar una frase: ";
+	getline(cin, f);
+	
+	e=f.size()-1;
+	
+	cout<<e;
+	return 0;
+}
+
+
